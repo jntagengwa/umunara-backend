8445eea5dd6487c8685b498db77a23f01e5616ef
Jean-Fidele Ntagegnwa <jean-fidele@Jean-Fideles-MacBook-Pro.local>
2020-12-07 19:08:44 +0000

    7:08

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 const winston = require("winston");
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
 const config = require("config");
 const app = express();
 
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
-//require("./startup/db")();
+require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
-app.use("/", (req, res) => {
-  MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
-    db.collection("Example", function (err, collection) {
-      collection.insert({ pageHits: "pageHits" });
-      db.collection("Example").count(function (err, count) {
-        if (err) throw err;
-        res.status(200).send("Page Hits: " + Math.floor(count / 2));
-      });
-    });
-  });
-});
-
-app.listen(port);
-console.log(`Server running at http://${db}:${port}/`);
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
 module.exports = server;
