e6c98ee90d2d494a483c32b5a6a174f1347aacb8
Jean-Fidele Ntagengwa <jntagengwa@gmail.com>
2020-12-09 20:10:51 +0000

    12/09/2020 key

--- models/user.js
+++ models/user.js
@@ -46,7 +46,6 @@ function validateUser(user) {
     name: Joi.string().min(2).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
-    isAdmin: Joi.boolean(),
   };
 
   return Joi.validate(user, schema);
